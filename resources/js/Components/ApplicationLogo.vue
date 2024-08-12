47051a6f285c96978a56dee68a237ca19615403c
Celso Nery <celso@karyon.com.br>
2024-08-12 12:56:41 -0300

    fix: Modifica logo ApplicationLogo.vue

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -3,9 +3,8 @@
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
         stroke="currentColor"
-        class="h-12 w-auto lg:h-16">
+        class="h-8 w-auto lg:h-10">
         <path
             stroke-linecap="round"
             stroke-linejoin="round"
